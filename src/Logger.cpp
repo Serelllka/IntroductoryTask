f229fe5d9ead4387a3d2f3109b975c2bd6e8572a
Serellka <vprogamervip@gmail.com>
2021-09-09 22:48:09 +0300

    feat: adds new Logger class

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,1 +1,18 @@
 #include "Logger.h"
+
+#include <ctime>
+
+Logger::Logger(std::ostream& output)
+    : output(output)
+{}
+
+void Logger::logGen(std::string &message)
+{
+    output << "string";
+}
+
+void Logger::logGen(std::string &message)
+{
+
+}
+
